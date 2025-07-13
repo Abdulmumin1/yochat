40feb73bdab263f4065eabd21e9b7cbab70f1a7e
abdulmumin1 <abdulmuminyqn@gmail.com>
2025-07-13 08:40:50 +0100

    update github actions

--- landing/src/routes/install/+server.js
+++ landing/src/routes/install/+server.js
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export const GET = () => {
 	return redirect(
 		308,
-		'https://raw.githubusercontent.com/abdulmumin1/yochat/refs/heads/master/install.sh'
+		'https://raw.githubusercontent.com/abdulmumin1/yochat/refs/heads/master/install'
 	);
 };
